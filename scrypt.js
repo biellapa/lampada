6ad69419f8c4a9b4bd24281cf250ed9f0abe86c5
gabriel cardoso da lapa <98766972+biellapa@users.noreply.github.com>
2022-06-03 15:05:35 -0300

    .

---
+++ scrypt.js
@@ -0,0 +1,8 @@
+alert("sou gay")
+
+const ligada = document.getElementById ( 'ligada' );
+const desligada = document.getElementById ( 'desligada' );
+const lamp = document.getElementById ( '' )
+
+
+
